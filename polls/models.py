588e78cb1d348ce6a8f9923e0e2fdbb5b4a6dfc8
rkdms0116 <rkdms0116@gmail.com>
2022-07-29 00:14:26 +0900

    polls model create

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
-from operator import mod
-from pyexpat import model
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -8,6 +9,12 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date puvlished')
+    # 객체 표시
+    def __str__(self):
+        return self.question_text
+    # 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 # 선택 텍스트와 투표 집계
 class Choice(models.Model):
@@ -15,3 +22,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
