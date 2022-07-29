cda8d53afc0f95f440de161e8a07efe43864ec4e
rkdms0116 <rkdms0116@gmail.com>
2022-07-29 14:55:12 +0900

    tutorial03 Use Form and generic view

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
-
+# URL 이름 공간
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
